c1763c7828e6ddb61e32403b434323f3238e8c1c
conmeonho <89299094+conmeonho@users.noreply.github.com>
2021-08-21 17:15:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Ê đồ láo kia!',
+    introDesc: `Huy Hoàng là cái đồ con heu mặp đjt đáng iu nhất mà tôi từng bít đó,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'cái đồ mặp đjt kia 😙',
+    desc: 'anh có thít tui khum ',
+    btnYes: 'quá tr là nhìu luôn đó <33',
+    btnNo: 'Không, m ảo tửn à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'Gửi cho bé <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'em bít mà hihi 🥰. đồ láo',
+    messDesc: 'anh là đồ láo toét đúng hem.',
+    btnAccept: 'đúng dị <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
